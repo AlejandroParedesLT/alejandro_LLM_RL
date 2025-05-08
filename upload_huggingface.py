0cd20ff70858eec67f31e8adef8fff493e2d5d06
Alejandro Paredes La Torre <alejandro.paredeslatorre@duke.edu>
2025-05-08 13:10:33 -0400

    Added details for upload and the cluster

---
+++ upload_huggingface.py
@@ -0,0 +1,16 @@
+from huggingface_hub import HfApi
+import os
+# Upload a folder to a Hugging Face Sp
+# Import environment variables
+from dotenv import load_dotenv
+load_dotenv()
+
+api = HfApi(token=os.getenv("HF_TOKEN"))
+
+# Upload all the content from the local folder to your remote Space.
+# By default, files are uploaded at the root of the repo
+api.upload_large_folder(
+    folder_path="./outputs",
+    repo_id="alejandroparedeslatorre/DeepSeek-R1-Distill-Qwen-1.5B-GRPO",
+    repo_type="model",
+)
